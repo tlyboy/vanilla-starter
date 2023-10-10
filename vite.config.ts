4dedfe57ec96f8e7b3c61442fc2d57b3d6320d63
Guany <1084072690@qq.com>
2023-10-10 15:11:38 +0800

    feat: add axios and unplugin-auto-import

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,37 @@
 import path from 'node:path'
 import { defineConfig } from 'vite'
+import AutoImport from 'unplugin-auto-import/vite'
 import UnoCSS from 'unocss/vite'
 
 export default defineConfig({
+  plugins: [
+    AutoImport({
+      imports: [
+        {
+          axios: [
+            ['default', 'axios'],
+          ],
+        },
+      ],
+      dirs: [
+        './src/utils',
+        './src/api',
+      ],
+    }),
+    UnoCSS(),
+  ],
   resolve: {
     alias: {
       '~/': `${path.resolve(__dirname, 'src')}/`,
     },
   },
-  plugins: [
-    UnoCSS(),
-  ],
+  server: {
+    proxy: {
+      '/github': {
+        target: 'https://api.github.com',
+        changeOrigin: true,
+        rewrite: path => path.replace(/^\/github/, ''),
+      },
+    },
+  },
 })
