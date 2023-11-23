075d82767993a1db5f4fb7f25020d219dbe4cb55
Guany <1084072690@qq.com>
2023-11-23 17:31:11 +0800

    chore: Refactor HTML and CSS code

--- src/main.ts
+++ src/main.ts
@@ -6,4 +6,26 @@ import nProgress from 'nprogress'
 nProgress.start()
 nProgress.done()
 
-document.querySelector<HTMLButtonElement>('#toggle-dark')?.addEventListener('click', toggleDark)
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <main
+    px-4 py-10
+    text="center gray-700 dark:gray-200"
+  >
+    <div>
+      <p>
+        🚀 Vanilla starter
+      </p>
+    </div>
+    <nav flex="~ gap-4" mt-6 justify-center text-xl>
+      <button id="toggle-dark" icon-btn title="切换深色模式">
+        <div i-carbon-sun dark:i-carbon-moon></div>
+      </button>
+
+      <a icon-btn rel="noreferrer" href="https://github.com/tlyboy/vanilla-starter" target="_blank" title="GitHub">
+        <div i-carbon-logo-github></div>
+      </a>
+    </nav>
+  </main>
+`
+
+document.querySelector<HTMLButtonElement>('#toggle-dark')!.addEventListener('click', toggleDark)
